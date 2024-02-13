ced57446d623bf94ba9accd7d24035d2ec5afe6e
MajkRitcherd <misutak87@gmail.com>
2024-02-13 15:35:28 +0100

    Parser options, constants

---
+++ SteganLib.Console/SteganLib.Console/Constants.cs
@@ -0,0 +1,68 @@
+﻿/**********************************************
+ * 
+ *          Created by Majk Ritcherd
+ * 
+ *********************************************/
+
+namespace SteganLib.Console
+{
+    /// <summary>
+    /// SteganLib.Console constants.
+    /// </summary>
+    public static class Constants
+    {
+        /// <summary>
+        /// Constants related to the <see cref="ParserOptions"/> class.
+        /// </summary>
+        public static class ParserOptions
+        {
+            /// <summary>
+            /// Constants related to the possible operations.
+            /// </summary>
+            public static class Operations
+            {
+                /// <summary>
+                /// Encode operation.
+                /// </summary>
+                public const string Encode = "enSteg";
+
+                /// <summary>
+                /// Decode operation.
+                /// </summary>
+                public const string Decode = "decSteg";
+            }
+
+            /// <summary>
+            /// Constants related to the possible argument keys.
+            /// </summary>
+            public static class ArgumentKeys
+            {
+                /// <summary>
+                /// Help key.
+                /// </summary>
+                public const string Help = "--help";
+
+                /// <summary>
+                /// Safe mode key.
+                /// </summary>
+                public const string SafeMode = "--safe";
+
+                /// <summary>
+                /// Message key.
+                /// </summary>
+                public const string Message = "--message";
+
+                /// <summary>
+                /// Image key.
+                /// </summary>
+                public const string Image = "--image";
+
+                /// <summary>
+                /// Password key.
+                /// </summary>
+                public const string Password = "--password";
+            }
+        }
+
+    }
+}
---
+++ SteganLib.Console/SteganLib.Console/Parser/ParserOptions.cs
@@ -0,0 +1,47 @@
+﻿/**********************************************
+ * 
+ *          Created by Majk Ritcherd
+ * 
+ *********************************************/
+
+using CO = SteganLib.Console.Constants.ParserOptions.Operations;
+using CAK = SteganLib.Console.Constants.ParserOptions.ArgumentKeys;
+
+namespace SteganLib.Console.Parser
+{
+    /// <summary>
+    /// Parser's options.
+    /// </summary>
+    public class ParserOptions
+    {
+        /// <summary>
+        /// Gets the all possible operations for parser.
+        /// </summary>
+        protected readonly List<string> Operations =
+        [
+            CO.Encode,  // enSteg
+            CO.Decode,  // decSteg
+        ];
+
+        /// <summary>
+        /// Gets all possible argument keys for parser (IMPORTANT: Extended in constructor).
+        /// </summary>
+        protected List<string> ArgumentKeys { get; private set; } =
+        [
+            CAK.Help,           // --help
+            CAK.Help[..3],      // --h
+
+            CAK.SafeMode,       // --safe
+            CAK.SafeMode[..3],  // --s
+
+            CAK.Message,        // --message
+            CAK.Message[..3],   // --m
+
+            CAK.Image,          // --image
+            CAK.Image[..3],     // --i
+
+            CAK.Password,       // --password
+            CAK.Password[..3],  // --p
+        ];
+    }
+}
